f50f54c1a37825bd9368bc5ea4e868af1c9030ab
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-04-17 22:21:44 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -19,9 +19,9 @@ else:
     # Fill the Values
     API_ID = 22427221
     API_HASH = "2785b4528c12682e515db3762463c126"
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
+    BOT_TOKEN = "6389119744:AAGWES7HoM1EdgtzoxtrnbX7TpoEJoENXzU"
+    DATABASE_URL = "mongodb+srv://xayonara:leoleo@cluster0.tkvlcza.mongodb.net/?retryWrites=true&w=majority"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
+    MUST_JOIN = "-1001779717901"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
